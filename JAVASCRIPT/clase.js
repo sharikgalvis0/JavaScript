b4b15ac91b6c0a47dce5e32c0b99290b4a0c7477
sharikgalvis0 <157921097+sharikgalvis0@users.noreply.github.com>
2024-09-04 12:42:07 -0500

    Add files via upload

---
+++ JAVASCRIPT/clase.js
@@ -0,0 +1,14 @@
+let nombre= "Sharik";
+let apellido ="Galvis";
+
+console.log(nombre)
+
+const usuario = "Mari"
+usuario = 33
+console.log(usuario)
+
+//concatenar
+let saludo="Hola ";
+
+let textocompleto = saludo + "Sharik " + "Tome agua";
+
